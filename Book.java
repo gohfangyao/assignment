48971d540ba357b3523e634df3898861c087fbab
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-05-09 23:12:47 +0800

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,19 @@
+public class Book
+{
+  public static void customer_base(){
+    System.out.println("Cover different reader in various field.");
+   }
+   
+   public static void material(){
+    System.out.println("The material of book include paper, cover, binding, printing colour");
+   }
+   
+   public static void content() {
+    System.out.println("This book has an interesting content.");
+   }
+   
+   public static void outlet() {
+    System.out.println("We can buy this book through Bookstores or Online website");
+   }
+   
+   }
