a0c1a829d5ea370a3ba39fb28f94d62383c047ec
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-05-09 23:13:08 +0800

    Create Book1.java

---
+++ Book1.java
@@ -0,0 +1,12 @@
+public class Book1 extends Book 
+{
+      String title = "Teaching in a Digital Age";
+      int numberOfPages = 250;
+      String author = "Zander Smith";
+      int published_date= 2018;
+      
+      public static void education(){
+   	      System.out.println("This book is for study purposes.");
+   }
+
+}
