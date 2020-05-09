780b8f094a45a6f36a0ed4b769512d0ef9abdf4b
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-05-09 23:13:26 +0800

    Create Book2.java

---
+++ Book2.java
@@ -0,0 +1,12 @@
+public class Book2 extends Book 
+{
+   String title = "The God Delusion";
+   int numberOfPages = 150;
+   String author = "Lauren W.";
+   int published_date= 2007;
+   
+   public static void religious_purposes(){
+      System.out.println("This book is for religious purposes.");
+   }
+
+}
