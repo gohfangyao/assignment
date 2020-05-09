909dcb9d0fc78d218300447cc827498558328a55
gohfangyao <55262977+gohfangyao@users.noreply.github.com>
2020-05-09 23:13:41 +0800

    Create Book3.java

---
+++ Book3.java
@@ -0,0 +1,13 @@
+public class Book3 extends Book 
+{
+	 String title = "Journey to Juno";
+	   int numberOfPages = 300;
+	   String author = "Galaxy Zach";
+	   int published_date= 2001;
+	   
+	   public static void entertainment(){
+	      System.out.println("This book is for entertainment purposes.");
+	      
+   }
+
+}
